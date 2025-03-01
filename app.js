0e9533b077f17f89abb495f7c5689dedeba47262
Bhumika-Raut <bhumikaraut59620@gmail.com>
2025-03-01 16:13:51 +0530

    Dio

---
+++ app.js
@@ -0,0 +1,39 @@
+const slide = document.querySelectorAll(".slide")
+var counter = 0;
+slide.forEach(
+    (slide,index) =>{
+        slide.style.left = `${index * 100}%`
+
+    }
+)
+
+const goPrev = () =>{
+    if (counter==0){
+        counter = slide.length-1;
+        slideImage();
+    } else{
+    counter--
+    slideImage()
+    }
+}
+
+
+
+const goNext = () => {
+    if(counter == slide.length-1){
+        counter = 0;
+        slideImage()
+    } else{
+    counter++
+    slideImage()
+    }
+}
+const slideImage = () => {
+    slide.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(-${counter*100}%)`
+
+        }
+        
+    )
+}
